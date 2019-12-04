09500904936cf7491e53fe3171a6fb1128bbe386
Arthur Ming <guobin.ming@pwc.com>
2019-12-04 09:57:47 +0800

    refactor(token): refactor create token api

--- api/main.go
+++ api/main.go
@@ -2,32 +2,21 @@ package api
 
 import (
 	service "damingerdai/address/services"
-	"time"
+	"os"
 
-	"github.com/dgrijalva/jwt-go"
 	"github.com/gin-gonic/gin"
 )
 
-var hmacSampleSecret = []byte("dam")
+var hmacSampleSecret []byte
 
-func CreateToken(c *gin.Context) {
-	username := c.GetHeader("username")
-	password := c.GetHeader("password")
+func init() {
+	secret := os.Getenv("Secret")
 
-	token := jwt.NewWithClaims(jwt.SigningMethodHS256, jwt.MapClaims{
-		"username": username,
-		"password": password,
-		"exp":      time.Date(2015, 10, 10, 12, 0, 0, 0, time.UTC).Unix(),
-	})
-
-	message, err := token.SignedString(hmacSampleSecret)
-
-	if err != nil {
-		c.JSON(400, err.Error())
-	} else {
-		c.JSON(200, message)
+	if len(secret) == 0 {
+		panic("env Secret is required")
 	}
 
+	hmacSampleSecret = []byte(secret)
 }
 
 func ListProvinces(c *gin.Context) {
--- routes/router.go
+++ routes/router.go
@@ -10,11 +10,12 @@ func NewRouter() *gin.Engine {
 	r := gin.Default()
 
 	v1 := r.Group("/api/v1")
-
-	v1.GET("ping", api.Ping)
-	v1.POST("token", api.CreateToken)
-	v1.GET("provinces", api.ListProvinces)
-	v1.GET("province/:id", api.GetProvince)
+	{
+		v1.GET("ping", api.Ping)
+		v1.POST("token", api.CreateToken)
+		v1.GET("provinces", api.ListProvinces)
+		v1.GET("province/:id", api.GetProvince)
+	}
 
 	return r
 }
