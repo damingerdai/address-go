ee5b4f986ca307633b4ade13c25a0b77b5a67ca5
Arthur Ming <guobin.ming@pwc.com>
2020-01-25 10:21:22 +0800

    feat(services:city): add city service

---
+++ internal/services/city.go
@@ -0,0 +1,28 @@
+package service
+
+import (
+	"damingerdai/address/internal/dao"
+	"damingerdai/address/internal/models"
+	"github.com/pkg/errors"
+	"strconv"
+)
+
+func ListCities() ([]*models.City, error) {
+	cities, err := dao.ListCities()
+	if err != nil {
+		return nil, errors.Wrap(err, "fail to get cities")
+	}
+	return cities, nil
+}
+
+func GetCity(id string) (*models.City, error) {
+	n, err := strconv.Atoi(id)
+	if err != nil {
+		return nil, err
+	}
+	city, err := dao.GetCity(n)
+	if err != nil {
+		return nil, err
+	}
+	return city, nil
+}
