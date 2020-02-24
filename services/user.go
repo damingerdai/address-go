ef43f893471e3229b0560c26697fb077033d0292
damingeridai <mingguobin@live.com>
2020-02-24 23:13:40 +0800

    feat: add create a user api

--- services/user.go
+++ services/user.go
@@ -1,32 +1,23 @@
 package service
 
-import (
-	"damingerdai/address/dao"
-	"damingerdai/address/internal/models"
-	"damingerdai/address/pkg/utils"
-	"strconv"
+// func CreateUser(user *models.User) (id int64, err error) {
+// 	id, err = utils.CreateID()
+// 	if err != nil {
+// 		return
+// 	}
+// 	user.ID = id
+// 	err = dao.CreateUser(user.ID, user.Username, user.Password)
+// 	return
+// }
 
-	"errors"
-)
+// func GetUser(id string) (*models.User, error) {
+// 	n, err := strconv.ParseInt(id, 10, 64)
+// 	if err != nil {
+// 		return nil, errors.New("bad request for user id")
+// 	}
+// 	return dao.GetUserById(n)
+// }
 
-func CreateUser(user *models.User) (id int64, err error) {
-	id, err = utils.CreateID()
-	if err != nil {
-		return
-	}
-	user.ID = id
-	err = dao.CreateUser(user.ID, user.Username, user.Password)
-	return
-}
-
-func GetUser(id string) (*models.User, error) {
-	n, err := strconv.ParseInt(id, 10, 64)
-	if err != nil {
-		return nil, errors.New("bad request for user id")
-	}
-	return dao.GetUserById(n)
-}
-
-func HasUser(user *models.User) (bool, error) {
-	return dao.HasUser(user)
-}
+// func HasUser(user *models.User) (bool, error) {
+// 	return dao.HasUser(user)
+// }
