ba6f2e8caae9ee7842ba6b8b7bc0200f0b556ad7
damingeridai <mingguobin@live.com>
2020-02-26 21:10:46 +0800

    fix: throw err when failing to creae user id

--- internal/services/user.go
+++ internal/services/user.go
@@ -3,10 +3,15 @@ package service
 import (
 	"damingerdai/address/internal/dao"
 	"damingerdai/address/pkg/utils"
+
+	"github.com/pkg/errors"
 )
 
 func CreateUser(username, password string) (int64, error) {
-	id, _ := utils.CreateID()
+	id, err := utils.CreateID()
+	if err != nil {
+		return 0, errors.Wrapf("fail to create user id: %s", err.Error())
+	}
 	err := dao.CreateUser(id, username, password)
 	if err != nil {
 		return 0, err
