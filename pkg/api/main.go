e358815f267b09ee420a7e09ad57e84e8891d604
damingeridai <mingguobin@live.com>
2020-02-28 21:35:38 +0800

    refactor: rename auth go

---
+++ pkg/api/main.go
@@ -0,0 +1,21 @@
+package api
+
+import (
+	"os"
+)
+
+var hmacSampleSecret []byte
+
+func init() {
+	secret := os.Getenv("Secret")
+
+	if len(secret) == 0 {
+		panic("env Secret is required")
+	}
+
+	hmacSampleSecret = []byte(secret)
+}
+
+func GetHmacSampleSecret() []byte {
+	return hmacSampleSecret
+}
