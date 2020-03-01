1c16a08a64f2162e1aac5898d33578713ee3500f
damingeridai <mingguobin@live.com>
2020-03-01 12:28:21 +0800

    chore: add header binding try

--- internal/models/user.go
+++ internal/models/user.go
@@ -2,6 +2,6 @@ package models
 
 type User struct {
 	ID       int64  `json:"id"`
-	Username string `json:"username"`
-	Password string `json:"password"`
+	Username string `json:"username" header:"username"`
+	Password string `json:"password" header:"password"`
 }
--- pkg/app/server.go
+++ pkg/app/server.go
@@ -4,17 +4,23 @@ import (
 	"damingerdai/address/pkg/routes"
 	"flag"
 	"fmt"
+	"github.com/joho/godotenv"
+	"log"
 	"os"
 )
 
 var port = flag.Int("port", 9999, "port")
 
 func Run() {
+	err := godotenv.Load(".env")
+	if err != nil {
+		log.Println("Error loading .env file")
+	}
 	flag.Parse()
 	address := fmt.Sprintf(":%d", *port)
 	fmt.Println(address)
 	r := routes.NewRouter()
-	err := r.Run(address)
+	err = r.Run(address)
 	if err != nil {
 		fmt.Fprintf(os.Stdout, "error: %v\n", err)
 	} else {
