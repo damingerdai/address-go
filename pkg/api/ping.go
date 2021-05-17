b817b9bcc69fd01293f73a0347f42157d2d22288
damingerdai <mingguobin@live.com>
2021-05-18 00:17:07 +0800

    build: add swagger ui support

--- pkg/api/ping.go
+++ pkg/api/ping.go
@@ -2,6 +2,10 @@ package api
 
 import "github.com/gin-gonic/gin"
 
+// @Summary ping api
+// @Produce  json
+// @Success 200 {string} string "成功"
+// @Router /api/v1/tags [get]
 func Ping(c *gin.Context) {
 	c.JSON(200, "pong")
 }
