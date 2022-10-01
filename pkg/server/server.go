00d4f4d8437a5b1e611b2c0293b40526473e2b06
Arthur Ming <guobin.ming@pwc.com>
2022-10-01 23:08:51 +0800

    feat: add server shut down support

--- pkg/server/server.go
+++ pkg/server/server.go
@@ -1,11 +1,14 @@
 package server
 
 import (
+	"context"
 	"flag"
 	"fmt"
 	"log"
 	"net/http"
 	"os"
+	"os/signal"
+	"syscall"
 	"time"
 
 	"github.com/damingerdai/address-go/pkg/routes"
@@ -30,10 +33,24 @@ func Run() {
 		ReadTimeout:  60 * time.Second,
 		WriteTimeout: 60 * time.Second,
 	}
-	err = s.ListenAndServe()
-	if err != nil {
-		fmt.Fprintf(os.Stdout, "error: %v\n", err)
-	} else {
-		fmt.Fprintln(os.Stdout, "run successfully")
+	go func() {
+		err = s.ListenAndServe()
+		if err != nil && err != http.ErrServerClosed {
+			fmt.Fprintf(os.Stdout, "error: %v\n", err)
+		} else {
+			fmt.Fprintln(os.Stdout, "run successfully")
+		}
+	}()
+
+	quit := make(chan os.Signal)
+	signal.Notify(quit, syscall.SIGINT, syscall.SIGTERM)
+	<-quit
+	fmt.Fprintln(os.Stdin, "shuting the server...")
+
+	ctx, cancel := context.WithTimeout(context.Background(), 5*time.Second)
+	defer cancel()
+	if err := s.Shutdown(ctx); err != nil {
+		fmt.Fprintln(os.Stderr, "Server force to shutdown:", err)
 	}
+	fmt.Fprintln(os.Stdin, "Server is existing")
 }
